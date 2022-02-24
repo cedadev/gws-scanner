79b62dafea8114bcf74f24f1283489efba44e929
Alex Manning <alexander.manning@stfc.ac.uk>
2022-02-24 13:56:48 +0000

    Add license.

---
+++ gws_volume_scanner/client/__init__.py
@@ -0,0 +1,6 @@
+"""Scanner for scanning filesystems data to elasticsearch."""
+
+__author__ = """Alex Manning"""
+__contact__ = "alexander.manning@stfc.ac.uk"
+__copyright__ = "Copyright 2022 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
